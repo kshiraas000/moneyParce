0761d19ae8d817ab06d88d1fa164c959f110d9c3
Aashna <aashna.kshirsagar@gmail.com>
2025-01-30 16:12:44 -0500

    login page made, also merged w/ Alli's branch

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import login as auth_login, authenticate
 from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
+def login(request):
+    template_data = {}
+    template_data['title'] = 'Login'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html',
+            {'template_data': template_data})
+    elif request.method == 'POST':
+        user = authenticate(
+            request,
+            username = request.POST['username'],
+            password = request.POST['password']
+        )
+        if user is None:
+            template_data['error'] = 'The username or password is incorrect.'
+            return render(request, 'accounts/login.html',
+                {'template_data': template_data})
+        else:
+            auth_login(request, user)
+            return redirect('home.index')
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
