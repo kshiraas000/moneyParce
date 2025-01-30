093963b3512ba6ea1238f120e8eb0e7c00f63d23
Aashna <aashna.kshirsagar@gmail.com>
2025-01-30 16:00:05 -0500

    user signup looks pretty now, still need to do login- aashna

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm, CustomErrorList
 from django.shortcuts import redirect
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
     if request.method == 'GET':
-        template_data['form'] = UserCreationForm()
+        template_data['form'] = CustomUserCreationForm()
         return render(request, 'accounts/signup.html',
             {'template_data': template_data})
     elif request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
             return redirect('home.index')
         else:
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
-                          {'template_data': template_data})
+                {'template_data': template_data})
