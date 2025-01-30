13a1bb6591de8b0a305feccd46a1cc6a6215965f
allibulot <allibulot@gmail.com>
2025-01-30 11:32:31 -0500

    chapter 4 done!!

--- movies/views.py
+++ movies/views.py
@@ -16,6 +16,11 @@ movies = [
     },
 ]
 def index(request):
+    template_data = {}
+    template_data['title'] = 'Movies'
+    template_data['movies'] = movies
+    return render(request, 'movies/index.html', {'template_data': template_data})
+def show(request, id):
     movie = movies[id -1]
     template_data = {}
     template_data['title'] = movie['name']
