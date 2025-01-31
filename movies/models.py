c311bbfd4f65e0d3eec75aa7c6d22cb66b395f44
Niki <nsahoo8@gatech.edu>
2025-01-30 20:44:04 -0500

    admin page created

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
