bd8d2bf22194026887d6c18cea47a50983bdbeb7
allibulot <allibulot@gmail.com>
2025-01-31 17:23:38 -0500

    fighting

--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,15 @@
 from django.db import models
-
-# Create your models here.
 class Movie(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     release_date = models.DateField()
     genre = models.CharField(max_length=100)
-
+    price = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='movie_images/')
     def __str__(self):
         return self.name
+
+
+
+
