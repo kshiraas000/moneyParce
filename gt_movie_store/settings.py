bed1ddd869955a0f1b490bec3ff82b83a3537ec6
allibulot <allibulot@gmail.com>
2025-02-11 16:37:53 -0500

    mysql

--- gt_movie_store/settings.py
+++ gt_movie_store/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "gt_movie_store.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gt-movie-store',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
