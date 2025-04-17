e82b8d7d99f672246c7a69e2002aec4cb2f3c8c2
Niki <nsahoo8@gatech.edu>
2025-04-17 12:28:58 -0400

    Added edit and delete on the transactions list page.

--- user_transactions/urls.py
+++ user_transactions/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('add/', views.add_transaction, name='add_transaction'),
     path('list/', views.transactions_list, name='transactions_list'),
-
+    path('edit/<int:pk>/', views.edit_transaction, name='edit_transaction'),
+    path('delete/<int:pk>/', views.delete_transaction, name='delete_transaction'),
 ]
--- user_transactions/views.py
+++ user_transactions/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Transaction
 from .forms import TransactionForm
 from django.contrib.auth.decorators import login_required
@@ -11,11 +11,30 @@ def add_transaction(request):
             transaction = form.save(commit=False)
             transaction.user = request.user
             transaction.save()
-            return redirect('transactions_list')  # make sure this URL name exists
+            return redirect('transactions_list')
     else:
         form = TransactionForm()
     return render(request, 'transactions/add_transaction.html', {'form': form})
 
+@login_required
 def transactions_list(request):
     transactions = Transaction.objects.filter(user=request.user).order_by('-date')
     return render(request, 'transactions/transactions_list.html', {'transactions': transactions})
+
+@login_required
+def edit_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    if request.method == 'POST':
+        form = TransactionForm(request.POST, instance=transaction)
+        if form.is_valid():
+            form.save()
+            return redirect('transactions_list')
+    else:
+        form = TransactionForm(instance=transaction)
+    return render(request, 'transactions/edit_transactions.html', {'form': form})
+
+@login_required
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk, user=request.user)
+    transaction.delete()
+    return redirect('transactions_list')
